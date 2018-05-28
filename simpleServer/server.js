b36818874e685b99c7d26aaa67bf427f133f3934
Jeff S <Slater.Jeff7@gmail.com>
2018-05-27 22:17:19 -0600

    fixed parser

--- simpleServer/server.js
+++ simpleServer/server.js
@@ -7,7 +7,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded{extended: false})
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use(express.static('public'))
